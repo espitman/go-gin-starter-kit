a7a364e49683a7dde9da8c55138ca12dd450fa1a
Saeed Heidari <saeed.heidari86@gmail.com>
2020-11-13 14:33:06 +0330

    clear: extra files & publishers

--- controller/ping/ping.go
+++ controller/ping/ping.go
@@ -1,18 +1,12 @@
 package pingController
 
 import (
-	"jettster/provider/rabbitmq"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func init() {
-	rabbitmq.CreatePublisher("ginTestExchange", "direct", true, "ginTestQueue")
-}
-
 func Ping(c *gin.Context) {
-	rabbitmq.Publish("ginTestExchange", "ginTestQueue", "hi from gin test")
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
