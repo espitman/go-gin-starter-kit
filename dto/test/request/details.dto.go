30d805381bf6d79d36f2a2f71b658b8106c84be5
s.heidari <s.heidari@jabama.com>
2021-11-13 16:26:13 +0330

    feat: remove test

--- dto/test/request/details.dto.go
+++
@@ -1,5 +0,0 @@
-package dto_test_request
-
-type Details struct {
-	ID string `uri:"id"`
-}
--- route/test.go
+++
@@ -1,7 +0,0 @@
-package route
-
-import testController "jettster/controller/test"
-
-func (t *T) TestRoutes() {
-	t.router.GET("/test/:id", testController.Ping)
-}
